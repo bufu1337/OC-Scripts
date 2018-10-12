65181d416a71e7a84209d43bd7f019a8bba78efe
bufu1337 <35832515+bufu1337@users.noreply.github.com>
2018-10-12 14:04:27 +0200

    second

--- StartCraft.lua
+++ StartCraft.lua
@@ -1,31 +1,58 @@
 local init = {}
 local shell = require("shell")
-
+local prog = "/home/crafting/"
 function init.getfiles()
   print("initializing files...")
-  local version
-  for line in io.lines(os.getenv("PWD") .. "/files") do
+  local Files
+  for line in io.lines("/home/crafting/files") do
     if version == nil then
       version = tonumber(line)
     else
       print("getting " .. line)
-      os.execute("wget -f 'https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/" .. line .."' '/home/crafting/" .. line .. "'")
+      os.execute("wget -f 'https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/" .. line .."' '" .. prog .. line .. "'")
     end
   end
   print("done")
 end
 
 function init.clone(repo, gotFilesList)
-	local previousVersion
-	local Version
-	if(exists("/home/crafting/"))then
-		if(file_exist("/home/crafting/files"))then
-			previousVersion = io.lines("/home/crafting/files")[1]
+	local pFiles = {}
+	local nFiles = {}
+	if(exists(prog))then
+		if(file_exist(prog .. "files"))then
+			pFiles = GetFiles(io.lines(prog .. "files"))
 		end
+	else
+		filesystem.makeDirectory(prog)
 	end
-	os.execute("wget -f 'https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/files' /home/crafting/newfiles")
+	os.execute("wget -f 'https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/files' '" .. prog .. "files'")
+	nFiles = GetFiles(io.lines(prog .. "newfiles"))
 	filesystem.remove("/home/crafting/newfiles")
 	init.getfiles()
+end
+--local linestest = {"Proxies.lua 0.001", "Convert.lua 0.001", "MoveItem.lua 0.001", "AutoCraft.lua 0.001"}
+local function GetFiles(lines)
+	local files = {}
+	for i, line in pairs(lines) do
+		local fcounter = 0
+		local filename
+		local f = {version=0}
+		for w in (line .. " "):gmatch("([^ ]*) ") do 
+			if(fcounter == 0)then
+				local c = 0
+				for x in w:gmatch("([^.]*).") do
+					if(c == 0)then filename = x end
+					c = c + 1
+				end
+			end
+			if(fcounter == 1)then
+				f.version = tonumber(w)
+			end
+			fcounter = fcounter +1
+		end
+		files[filename] = f
+	end
+	return files
 end
 
 
