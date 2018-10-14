828dd0a0109205d6b00aa221ab33fd0fbeec8cfa
bufu1337 <a.skripkin@hotmail.de>
2018-10-15 00:24:57 +0200

    Update screenchange.lua

--- screenchange.lua
+++ screenchange.lua
@@ -1,11 +1,15 @@
 local thread = require("thread")
 local c = require("component")
+local rs = c.block_refinedstorage_grid_2
+local sides = require("sides")
 c.modem.open(111)
 print("screenchange init")
 thread.create(function()
     while true do
         local _, _, _, _, _, message = event.pull("modem_message")
-		print(message)
+		local rs_item = rs.getItem({name="minecraft:stick"}, true)
+		local dropped = rs.extractItem(rs_item, 32, sides.down)
+		print(dropped)
 	end
 end)
 print("screenchange - thread started")
