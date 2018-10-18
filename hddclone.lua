31cb2dd8fc752a2eca32a2488ecb77baa57082ca
bufu1337 <a.skripkin@hotmail.de>
2018-10-18 23:55:54 +0200

    Update hddclone.lua

---
+++ hddclone.lua
@@ -0,0 +1,19 @@
+local fs = require("filesystem")
+local shell = require("shell")
+ 
+local args, options = shell.parse(...)
+if #args < 2 then
+  io.write("Usage:\n")
+  io.write("  hddclone <from> <to>")
+  return
+end
+
+local fromstr = args[1]
+local from = { path = "/mnt/" .. args[1] }
+
+for times = 2, #args, 1 do
+    local to = { path = "/mnt/" .. args[times] } 
+    io.write("Copying files from " .. from.path .. " to " .. to.path .. ".\n")
+    shell.execute("cp -v -r " .. from.path .. "* " .. to.path)
+    io.write("\27[32mDone!")
+end
