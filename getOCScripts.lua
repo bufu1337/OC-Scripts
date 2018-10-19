509295573c34a3936784a139ebc0de32da976208
bufu1337 <35832515+bufu1337@users.noreply.github.com>
2018-10-19 18:17:26 +0200

    Add files via upload

--- getOCScripts.lua
+++ getOCScripts.lua
@@ -8,5 +8,6 @@ for i,j in pairs(filesystem.findNode("mnt").children.mnt.children) do
 end
 os.execute("mount BufuScripts /bufu")
 os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/StartCraft.lua" .. "?" .. math.random() .. " /home/crafting/StartCraft.lua")
+os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/MainFunctions.lua" .. "?" .. math.random() .. " /home/MainFunctions.lua")
 os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/screenchange.lua" .. "?" .. math.random() .. " /home/screenchange.lua")
 os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/autorun.lua" .. "?" .. math.random() .. " /autorun.lua")
