b3970f0a3c95e71fe33b4129fe68d15c75dd7e58
bufu1337 <35832515+bufu1337@users.noreply.github.com>
2018-10-29 17:41:48 +0100

    Add files via upload

--- getMaxDamage.lua
+++ getMaxDamage.lua
@@ -1,4 +1,5 @@
 local convert = require("crafting/Convert")
+local filesystem = require("filesystem")
 local hs = {}
 local function hasDamage(item)
   local item_c = convert.TextToItem(item)
@@ -11,11 +12,15 @@ local function hasDamage(item)
 end
 local function getMax(item)
   local item_c = convert.TextToItem(item)
-  local m = require("crafting/Items/max/" .. item_c.mod)
-  if m[item] ~= nil then
-    return m[item]
+  if filesystem.exists("/home/crafting/Items/max/" .. item_c.mod .. ".lua") then
+	  local m = require("crafting/Items/max/" .. item_c.mod)
+	  if m[item] ~= nil then
+		return m[item]
+	  else
+		return 0
+	  end
   else
-    return 0
+	return 0
   end
 end
 hs.hasDamage = hasDamage
