b1beb97ed32748593ce2355ed2da8ccaa51e7916
bufu1337 <35832515+bufu1337@users.noreply.github.com>
2018-11-28 17:40:23 +0100

    Add files via upload

--- Builder/model.lua
+++ Builder/model.lua
@@ -1,12 +1,13 @@
-local os = require("os")
-local filesystem = require("filesystem")
+--local os = require("os")
+--local filesystem = require("filesystem")
 local mf = require("MainFunctions")
 local gitrepo = "https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Builder/"
-local wp = "/home/Builder/Models/"
+--local wp = "/home/Builder/Models/"
+local wp = "C:/Users/alexandersk/workspace/OC-Scripts/src/Builder/Models/"
 local model = {}
 local model_list = {}
 local function listNewModels()
-  os.execute("wget -f " .. gitrepo .. "model_list.lua" .. "?" .. math.random() .. " /home/Builder/model_list.lua")
+  --os.execute("wget -f " .. gitrepo .. "model_list.lua" .. "?" .. math.random() .. " /home/Builder/model_list.lua")
   model_list = require("Builder/model_list")
   print("List of models in builder")
   mf.printx(model_list)
@@ -19,21 +20,21 @@ local function checkLoadedModel()
   end
 end
 local function listModels()
-  if filesystem.exists("/home/Builder/model_list.lua") == false then
-    listNewModels()
-  else
+  --if filesystem.exists("/home/Builder/model_list.lua") == false then
+    --listNewModels()
+  --else
     model_list = require("Builder/model_list")
     print("List of models in builder")
     mf.printx(model_list)
-  end
+  --end
 end
 local function getNewModel(modelname)
-  os.execute("wget -f " .. gitrepo .. "Models/" .. modelname .. ".model.lua" .. "?" .. math.random() .. " " .. wp .. modelname .. ".model.lua")
+  --os.execute("wget -f " .. gitrepo .. "Models/" .. modelname .. ".model.lua" .. "?" .. math.random() .. " " .. wp .. modelname .. ".model.lua")
 end
 local function getModel(modelname)
-  if filesystem.exists(wp .. modelname .. ".model.lua") == false then
-    os.execute("wget -f " .. gitrepo .. "Models/" .. modelname .. ".model.lua" .. "?" .. math.random() .. " " .. wp .. modelname .. ".model.lua")
-  end
+  --if filesystem.exists(wp .. modelname .. ".model.lua") == false then
+    --os.execute("wget -f " .. gitrepo .. "Models/" .. modelname .. ".model.lua" .. "?" .. math.random() .. " " .. wp .. modelname .. ".model.lua")
+  --end
 end
 local function mirrorLevel(level, vertical)
   local new_level = {}
@@ -150,10 +151,10 @@ local function getLevel(level)
   if checkLoadedModel() then
     local levstr = string.sub(tostring(level + 1000), 2)
     local i = 1
-    local levelpath = model.loadedModel.name:sub(1,#model.loadedModel.name - 6) .. "/" .. levstr
-    if filesystem.exists(wp .. levelpath) == false then
-      os.execute("wget -f " .. gitrepo .. "Models/" .. levelpath .. "?" .. math.random() .. " " .. wp .. levelpath)
-    end
+    local levelpath = model.loadedModel.name .. "/" .. levstr
+    --if filesystem.exists(wp .. levelpath) == false then
+      --os.execute("wget -f " .. gitrepo .. "Models/" .. levelpath .. "?" .. math.random() .. " " .. wp .. levelpath)
+    --end
     for line in io.lines(wp .. levelpath) do
       lev[i] = line
       i = i + 1
@@ -163,21 +164,32 @@ local function getLevel(level)
   return lev
 end
 local function loadModelEx(modelname, rotation, startpoint)
-  getModel(modelname)
-  model.loadedModel = require("Builder/Models/" .. model_list[mf.getIndex(modelname .. ".model.lua")])
-  model.loadedModel.rotation = rotation
-  model.loadedModel.startpoint = startpoint
-  model.loadedModel.size = rotateLevel(rotateLevel(mirrorLevel(getLevel(1), false), 3)[1], rotation)[2]
-  model.loadedModel.levelsCount = #model.loadedModel.levels
+  --getModel(modelname)
+  model.loadedModel = require("Builder/Models/" .. modelname)
+  if model.loadedModel.rotation == nil then model.loadedModel.rotation = rotation end
+  if model.loadedModel.startpoint == nil then model.loadedModel.startpoint = startpoint end
+  if model.loadedModel.size == nil then model.loadedModel.size = rotateLevel(rotateLevel(mirrorLevel(getLevel(1), false), 3)[1], model.loadedModel.rotation)[2] end
+  if model.loadedModel.levelsCount == nil then model.loadedModel.levelsCount = #model.loadedModel.levels end
+  for i = 1, model.loadedModel.levelsCount, 1 do
+    if model.loadedModel.levels[i].completed == nil or model.loadedModel.levels[i].completed == false then
+      model.loadedModel.levels[i].completed = false
+      if model.loadedModel.levels[i].rowComplete == nil then
+        model.loadedModel.levels[i].rowComplete = {}
+        for j = 1, model.loadedModel.size.y, 1 do
+          model.loadedModel.levels[i].rowComplete[j] = false
+        end
+      end
+    end
+  end
 end
 local function loadModel(modelname, rotation, startpoint)
   listModels()
-  if mf.contains(model_list, modelname .. ".model.lua") then
+  if mf.contains(model_list, modelname .. ".lua") then
     loadModelEx(modelname, rotation, startpoint)
     return true
   else
     listNewModels()
-    if mf.contains(model_list, modelname .. ".model.lua") then
+    if mf.contains(model_list, modelname .. ".lua") then
       loadModelEx(modelname, rotation, startpoint)
       return true
     else
@@ -197,28 +209,49 @@ end
 local function unloadModel()
   model.loadedModel = nil
 end
-local function getModelLevel(level, starting, ending)
+local function setLevelComplete(level)
+
+end
+local function setLevelRowComplete(level, row)
+
+end
+local function getModelLevelEx(level, starting, ending)
   local newlevel = rotateLevel(rotateLevel(mirrorLevel(getLevel(level), false), 3)[1], model.loadedModel.rotation)[1]
+  print("---------------------------------------------- newlevel ----------------------------------------------")
+  mf.printx(newlevel)
+  print("")
   local lev = {}
   if checkLoadedModel() then
+    local c = 1
     for j, l in pairs(newlevel) do
-      lev[j] = {}
       if j >= starting and j <= ending then
+        lev[c] = {}
+        local h = 1
         for b = 1, #l, 1 do
           local letter = l:sub(b,b)
-          if letter == "-" or letter == " " then
-            lev[j][b] = {}
-          else
-            lev[j][b] = {name=model.loadedModel.mats[letter], placed=false}
+          if letter ~= "-" and letter ~= " " then
+            lev[c][h] = {name=model.loadedModel.mats[letter], pos={x=(model.loadedModel.startpoint.x + b - 1),y=(model.loadedModel.startpoint.y + j - 1),z=(model.loadedModel.startpoint.z + level - 1)}}
+            h = h + 1
           end
         end
+        c = c + 1
       end
     end
   end
   return lev
 end
 local function getModelLevel(level)
-  return getModelLevel(level, 1, model.loadedModel.y)
+  return getModelLevelEx(level, 1, model.loadedModel.startpoint.y)
 end
-
+local model_loaded = loadModel("Brick Mansion - by ND63319", 2, {x=10,y=30,z=65})
+if model_loaded then
+  print("")
+  print("---------------------------------------------- loadedModel ----------------------------------------------")
+  mf.printx(model.loadedModel)
+  print("")
+  print("---------------------------------------------- getModelLevel 1 ----------------------------------------------")
+  mf.printx(getModelLevelEx(1, 5, 6))
+else
+  print("Cant load model: " .. "Brick Mansion - by ND63319")
+end
 return model
