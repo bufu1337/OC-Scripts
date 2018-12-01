6b29810b8980a3a4d567d800126c9217240af1be
bufu1337 <a.skripkin@hotmail.de>
2018-12-01 03:10:26 +0100

    Update cyclic.lua

--- Crafter/Items/cyclic.lua
+++ Crafter/Items/cyclic.lua
@@ -402,7 +402,6 @@ return {
     mountainmobs_jj_boulderblastscepter={maxCount=8, craftCount=1},
     mountainmobs_jj_mountainspawn={maxCount=8, craftCount=1},--.withTag({EntityTag: {id: mountainmobs:barghest}})
     mountainmobs_jj_mountainspawn={maxCount=8, craftCount=1},--.withTag({EntityTag: {id: 
-    mountainmobs_jj_jabberwock={maxCount=8, craftCount=1},
     mountainmobs_jj_mountainspawn={maxCount=8, craftCount=1},--.withTag({EntityTag: {id: mountainmobs:beholder}})
     mountainmobs_jj_mountainspawn={maxCount=8, craftCount=1},--.withTag({EntityTag: {id: mountainmobs:troll}})
     mountainmobs_jj_mountainspawn={maxCount=8, craftCount=1},--.withTag({EntityTag: {id: mountainmobs:wildkin}})
