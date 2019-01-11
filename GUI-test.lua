bbf10fe6b2889b45cde0aed1eb487aa9cd5fc8ae
bufu1337 <a.skripkin@hotmail.de>
2019-01-11 01:16:58 +0100

    Update GUI-test.lua

--- GUI-test.lua
+++ GUI-test.lua
@@ -4,22 +4,32 @@ t.mf = require("MainFunctions")
 t.rs = require("rstorages")
 
 t.app = t.gui.application()
---t.app:addChild(t.gui.panel(1, 1, t.app.width, t.app.height, 0x2D2D2D))
+t.app:addChild(t.gui.panel(1, 1, t.app.width, t.app.height, 0x2D2D2D))
 
-local txt = t.app:addChild(t.gui.text(3, 2, 0xFFFFFF, "Hello, world!"))
-local verticalList = t.app:addChild(t.gui.list(3, 2, 25, 49, 1, 0, 0xE1E1E1, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false))
-for i,j in pairs(t.mf.getSortedKeys(t.rs)) do
-  verticalList:addItem(i).onTouch = function()
-    txt.text = t.mf.serial.serialize(verticalList:getItem(verticalList.selectedItem), true)
-    t.mf.WriteObjectFile(verticalList:getItem(verticalList.selectedItem), "/home/listitem.lua")
+t.txt = t.app:addChild(t.gui.text(60, 1, 0xFFFFFF, t.mf.getSortedKeys(t.rs.rstorages)[1]))
+t.txt2 = t.app:addChild(t.gui.text(60, 2, 0xFFFFFF, ""))
+local bc = t.mf.getCount(t.rs.storages)
+local bb = math.floor(50 / bc)
+local by = math.floor(50 - (bb * bc)) + 1
+t.rsList = t.app:addChild(t.gui.list(3, by, 20, (bb * bc), bb, 0, 0xE1E1E1, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false))
+for i,j in pairs(t.mf.getSortedKeys(t.rs.rstorages)) do
+  t.rsList:addItem(j).onTouch = function()
+    t.txt.text = tostring(t.rsList:getItem(t.rsList.selectedItem).text)
   end
-  --table.insert(textBox.lines, i)
 end
-
---local textBox = t.app:addChild(t.gui.textBox(2, 2, 32, 40, 0xEEEEEE, 0x2D2D2D, {}, 1, 1, 0))
---for i,j in pairs(t.rs) do
---  table.insert(textBox.lines, i)
---end
+bc = t.mf.getCount(t.rs.monitor)
+bb = math.floor(50 / bc)
+by = math.floor(50 - (bb * bc)) + 1
+t.mList = t.app:addChild(t.gui.list(27, by, 15, (bb * bc), bb, 0, 0xE1E1E1, 0x4B4B4B, 0xD2D2D2, 0x4B4B4B, 0x3366CC, 0xFFFFFF, false))
+for i = 1, #t.rs.monitor, 1 do
+  t.mList:addItem("RS Monitor " .. i).onTouch = function()
+    t.txt2.text = tostring(t.mList:getItem(t.rsList.selectedItem).text)
+  end
+end
+t.app:addChild(t.gui.roundedButton(60, 46, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Exit")).onTouch = function()
+  t.app:draw(false)
+  t.app:stop()
+end
 
 t.app:draw(true)
 t.app:start()
