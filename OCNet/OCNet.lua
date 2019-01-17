fc05c9d905db0680f263d23fe71219420ff5699c
bufu1337 <35832515+bufu1337@users.noreply.github.com>
2019-01-17 15:53:06 +0100

    Add files via upload

--- OCNet/OCNet.lua
+++ OCNet/OCNet.lua
@@ -1,4 +1,4 @@
-local oc = {mf=require("MainFunctions"), tunnels={}, receivers={}}
+local oc = {mf=require("MainFunctions"), main_oc=false, tunnels={}, receivers={}}
 
 for i,j in pairs(oc.mf.filesystem.findNode("mnt").children.mnt.children) do
   if (j.fs.spaceTotal() == 524288 and oc.mf.filesystem.findNode("mnt").children.mnt.children[i].fs.getLabel() == nil) then
@@ -15,10 +15,24 @@ end
 oc.modem = oc.mf.component.modem
 oc.mf.component.modem.close(478)
 oc.mf.component.modem.open(478)
-
+function oc.findMain()
+  oc.modem.broadcast(478, oc.mf.serial.serialize({OCNet={toSystem="OCNet", "getMain"}}))
+  local _, localAddress, remoteAddress, _, _, data = oc.mf.event.pull(5,"modem_message")
+  if remoteAddress == nil then
+    print("No main OCNet Server found. Setting this to Main-Server.")
+    oc.main_oc = true
+  else
+  
+  end
+end
 while true do
    local _, localAddress, remoteAddress, _, _, data = oc.mf.event.pull("modem_message")
    oc.mf.thread.create(function(localAddress, remoteAddress, data)
-     
+     data = oc.mf.serial.unserialize(data)
+     if oc.mf.containsKey(data, "OCNet") then
+        if data.OCNet.toSystem == "OCNet" then
+        
+        end
+     end
    end, localAddress, remoteAddress, data)
 end
