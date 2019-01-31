4e78d1d295a3201519e1fd4cc660e9ae33b6ef10
bufu1337 <a.skripkin@hotmail.de>
2019-01-31 17:15:53 +0100

    Update hasPatternCheck.lua

--- Crafter/hasPatternCheck.lua
+++ Crafter/hasPatternCheck.lua
@@ -1,17 +1,19 @@
 local shell = require("shell")
 local function CheckPattern(param)
-    local items = require("home/crafting/Items/" .. param)
-    local prox = require("home/crafting/Proxies")
-    local convert = require("home/crafting/Convert")
+    local filesystem = require("filesystem")
+    local os = require("os")
+    os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/ItemsNew/" .. param .. ".lua" .. "?" .. math.random() .. " /home/bufu/GetCrafter.lua")
+    local items = require("home/" .. param)
+    local prox = require("home/Proxies")
+    local convert = require("home/Convert")
+    local mf = require("home/MainFunctions")
     local c = require("component")
     local rs = c.proxy(prox.GetProxyByName(param, "craft"))
-    local checkFile = io.open("home/crafting/Items/" .. param .. "_check.lua", "w")
+    local newItems = {}
     for i,j in pairs(items) do
-        local h = tostring(rs.hasPattern(convert.TextToItem(i)))
-        print(i .. " = " .. h)
-        checkFile:write(i .. " = " .. h .. "\n")
+        newItems[i] = rs.hasPattern(convert.TextToItem(i))
     end
-    checkFile:close()
+    filesystem.remove("/home/" .. param .. ".lua")
 end
 local args = shell.parse( ... )
 if args[1] ~= nil and args[1]:find("hasPatternCheck") == nil then
