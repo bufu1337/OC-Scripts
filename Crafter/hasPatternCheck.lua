08a416080df5474cf97864f58fd4e99c3bba40c6
bufu1337 <a.skripkin@hotmail.de>
2019-02-02 00:16:09 +0100

    Update hasPatternCheck.lua

--- Crafter/hasPatternCheck.lua
+++ Crafter/hasPatternCheck.lua
@@ -11,41 +11,12 @@ local function CheckPattern(param)
     local rs = c.proxy(prox.GetProxyByName(param, "craft"))
     local newItems = {}
     
-    local itemcounter = 0
-    local co = 1
-    local iarr = {}
-    local th = {}
-    for i,j in pairs(items) do
-        itemcounter = itemcounter + 1
-        if (itemcounter > 50) then
-          itemcounter = 1
-          co = co + 1
-        end
-        if iarr[co] == nil then
-            iarr[co] = {}
-        end
-        iarr[co][itemcounter] = i
-    end
-    local ttable = {}
-    for v = 1, co, 1 do
-      th[v] = mf.thread.create(function(o, u)
-        for v = 1, u, 1 do
-          mf.os.sleep(v)
-        end
-        for i,j in pairs(o) do 
-            newItems[i] = rs.hasPattern(convert.TextToItem(i))
-        end
-      end, iarr[v], v)
-      table.insert(ttable, th[v])
-    end
-    mf.thread.waitForAll(ttable)
-
-    --[[for i,j in pairs(items) do 
+    for i,j in pairs(items) do 
         print(i)
         newItems[i] = rs.hasPattern(convert.TextToItem(i))
         print(i .. " = " .. tostring(newItems[i]))
-    end]]
-    mf.WriteObjectFile(newItems, "/home/" .. param .. "_check.lua")
+    end
+    mf.WriteObjectFile(newItems, "/home/patternCheck/" .. param .. "_check.lua")
     filesystem.remove("/home/" .. param .. ".lua")
 end
 local args = shell.parse( ... )
