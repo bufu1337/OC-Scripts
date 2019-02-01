38aec4efabcff268667d8ea6b6b8bd036d0a0ef6
bufu1337 <a.skripkin@hotmail.de>
2019-02-02 00:25:29 +0100

    Update hasPatternCheck.lua

--- Crafter/hasPatternCheck.lua
+++ Crafter/hasPatternCheck.lua
@@ -1,25 +1,23 @@
 local shell = require("shell")
-local function CheckPattern(param)
-    local filesystem = require("filesystem")
-    local os = require("os")
-    os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/ItemsNew/" .. param .. ".lua" .. "?" .. math.random() .. " /home/" .. param .. ".lua")
+local prox = require("Proxies")
+local convert = require("Convert")
+local mf = require("MainFunctions")
+local hpc = {}
+function hpc.CheckPattern(param)
+    mf.os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/ItemsNew/" .. param .. ".lua" .. "?" .. math.random() .. " /home/" .. param .. ".lua")
     local items = require(param)
-    local prox = require("Proxies")
-    local convert = require("Convert")
-    local mf = require("MainFunctions")
-    local c = require("component")
-    local rs = c.proxy(prox.GetProxyByName(param, "craft"))
+    local rs = mf.component.proxy(prox.GetProxyByName(param, "craft"))
     local newItems = {}
-    
     for i,j in pairs(items) do 
         print(i)
         newItems[i] = rs.hasPattern(convert.TextToItem(i))
         print(i .. " = " .. tostring(newItems[i]))
     end
     mf.WriteObjectFile(newItems, "/home/patternCheck/" .. param .. "_check.lua")
-    filesystem.remove("/home/" .. param .. ".lua")
+    mf.filesystem.remove("/home/" .. param .. ".lua")
 end
 local args = shell.parse( ... )
 if args[1] ~= nil and args[1]:find("hasPatternCheck") == nil then
     CheckPattern(args[1])
 end
+return hpc
