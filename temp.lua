57c70144eba69123e9c25a1dee4ce3fb5db29c81
bufu1337 <a.skripkin@hotmail.de>
2020-02-05 02:22:06 +0100

    k

---
+++ temp.lua
@@ -0,0 +1,82 @@
+gp = require("home/GetPattern")
+gp.GetPattern("forestry", true)
+
+gp.items = {}; gp.GetPattern("storage", true); gp.items = {}; gp.GetPattern("nuclear", true); gp.items = {}; gp.GetPattern("food", true); gp.items = {}; gp.GetPattern("immersive", true); gp.items = {}; gp.GetPattern("fiifex", true); gp.items = {}; gp.GetPattern("thermal", true); gp.items = {}; gp.GetPattern("industrial", true);  gp.items = {}; gp.GetPattern("treborn", true); gp.items = {}; gp.GetPattern("rftools", true); gp.items = {}; gp.GetPattern("metals", true); gp.items = {}; gp.GetPattern("modern", true);  gp.items = {}; gp.GetPattern("actadd", true); gp.items = {}; gp.GetPattern("rocketry", true); gp.items = {}; gp.GetPattern("biomes", true); gp.items = {}; gp.GetPattern("botania", true); gp.items = {}; gp.GetPattern("mystical", true); gp.items = {}; gp.GetPattern("biblio", true); gp.items = {}; gp.GetPattern("fairy", true); gp.items = {}; gp.GetPattern("reliq", true); gp.items = {}; gp.GetPattern("terra", true); gp.items = {}; gp.GetPattern("appen", true); gp.items = {}; gp.GetPattern("btrees", true); gp.items = {}; gp.GetPattern("aether", true); gp.items = {}; gp.GetPattern("plants", true);  
+gp = require("home/GetPattern"); gp.items = {}; gp.items = {}; gp.GetPattern("minewatch", true); gp.items = {}; gp.GetPattern("railroading", true);
+
+gp.GetPattern("cyclic", true); gp.items = {}; gp.GetPattern("ceramics", true); gp.items = {}; gp.GetPattern("atl", true); gp.items = {}; 
+
+gp = require("home/GetPattern"); gp.items = {}; gp.GetPattern("forestry", true); os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/ItemsFullAll/2/conquest.lua" .. "?" .. math.random() .. " /mnt/58e/home/Crafter/ItemsAll/conquest.lua"); os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/ItemsFullAll/2/others.lua" .. "?" .. math.random() .. " /mnt/58e/home/Crafter/ItemsAll/others.lua");
+gp.items = {}; gp.GetPattern("fiifex", true); gp.items = {}; gp.GetPattern("plants", true); gp.items = {}; gp.GetPattern("rocketry", true); gp.items = {}; gp.GetPattern("immersive", true); gp.items = {}; gp.GetPattern("modern", true); 
+gp = require("home/GetPattern"); gp.items = {}; gp.GetPattern("terra", true); 
+
+
+
+gp.items = {}; gp.GetPattern("more", true);
+gp.items = {}; gp.GetPattern("flat", true);
+gp.items = {}; gp.GetPattern("deco", true);
+gp.items = {}; gp.GetPattern("future", true);
+gp.items = {}; gp.GetPattern("chisel", true);
+gp.items = {}; gp.GetPattern("bits", true);
+gp.items = {}; gp.GetPattern("pillar", true);
+gp.items = {}; gp.GetPattern("chimneys", true);
+
+
+os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/ItemsFullAll/terra.lua" .. "?" .. math.random() .. " /mnt/a9f/home/Crafter/ItemsAll/terra.lua");
+os.execute("wget -f https://raw.githubusercontent.com/bufu1337/OC-Scripts/master/Crafter/GetItemFiles.lua" .. "?" .. math.random() .. " /home/Crafter/GetItemFiles.lua");
+
+minecraft	"5e641952-57b9-4d2c-bdc4-84c3169c9003",
+draconic	"d24e6b8e-ff7b-44ea-89b4-86a73b6b9625",  
+conquest	"a40d3a48-9744-4d54-99d3-87c76fd4ba93",
+mekanism	"d2d897c3-e2a9-4fdf-a577-fe3b0dfae27a",
+utilities	"b9284235-0046-435d-a7eb-fd1f5b2c6bad",
+environ	"0d93120a-568c-4112-a1dd-255ab5e26684",
+storage	"dfb57f87-bd70-4562-9ec1-22156a464a96",
+nuclear	"b333b242-2daa-459c-84b0-c0f9b0b0bbe9",
+forestry	"cae64dcb-1234-4952-aee0-21972148db72",
+food	"8fc3a825-7e28-4837-90e1-4d4c81509270",
+immersive	"1cc5a8a0-26da-4edd-995a-866bc95c184f",
+railroad	"52a18662-2e76-44a7-a999-7debbbf74084",
+others	"cd1ea590-5105-4d81-abe2-9864207eae4d",
+fiifex	"1fd5006c-a302-4943-bb41-637eb00ed542",
+thermal	"d59fa1d2-8dda-4dba-89c1-45a0fdb89127",
+industrial	"edaa9125-0d0b-422a-bf67-54e9035d8d6c",
+treborn	"2a7e3cfc-9b8e-48b2-a28e-9150a69796fc",
+rftools	"8504ecf4-9fea-40c2-86c5-9f6480be785b",
+metals	"bb206f2e-c8c6-4c84-9597-4e8c1f1e5b50",
+modern	"01aa486d-7160-4fd9-8eb4-9a4b01e673c7",
+actadd	"b9e56557-ffc7-42d3-bf8e-395c365ee1d0",
+rocketry	"5c87e4c8-8126-40c0-a831-4f55d54d310c",
+biomes	"947d640f-de6a-4393-a204-9e68acc67885",
+botania	"cdbb54c8-bc48-4094-a773-a69192013e62",
+mystical	"9264e574-ff05-4e98-bfc8-c350ee107460",
+biblio	"c0487111-c1ff-44b6-815c-b22ec4b10f4a",
+fairy	"ab4d7c15-3ac1-435a-8432-148cd3ecbb6c",
+reliq	"65deff17-f43f-481f-b041-f958166f6ef8",
+terra	"fcfc1925-82ce-40fc-b99c-97b5329df2a5",
+appen	"8f381a1f-917a-4a9f-bc11-97261ac971b5",
+btrees	"169a4831-fdca-4ee7-91a2-a9a7066a7f31",
+binnie	"b14d7bfd-db09-455a-8189-f3a81715475f",
+aether	"da00087f-d650-43cb-8880-8cf022f230b6",
+plants	"963ccc48-7c46-45b8-9c5b-7a32b8f64352",
+cyclic	"a2dfecff-b3a4-4bd7-b4d9-068e72ac2185",
+tinkers	"92e2ce05-416d-4e6f-961a-0100e523a272",
+ceramics	"5f79d96a-06f5-4d59-813e-0ea958ea338a",
+atl	"9c86c94a-84c5-4a3a-9876-8bd9e0ddaa3a",
+deco	"d9db7a98-b5b2-4b78-b93a-0fdf7b6583c6",
+chisel	"033a0f4a-b1ab-4c80-a76f-5a592459c926",
+bits	"0e62359f-929d-4b17-b2bb-d1b64a23f7e3",
+flat	"bef99875-4b87-4874-8deb-7b18577c66a2",
+pillar	"14550cb5-4cdb-45ba-b91e-2608e243ae67",
+more	"a08ae5fc-c0ad-4598-9a6c-f4a2b74d0128",
+chimneys	"df0daf1b-f852-4639-b203-95a496ca7f1f",
+minewatch	"e0184266-3df9-4b63-b052-830060a9b8a3",
+future	"22997dcf-62d9-43a1-a8f7-c5a1b59b5b71",
+        itemsort = "ae451f31-8a79-4ca0-8b40-9a534ba94511",
+        fluid = "56338f1c-21de-4400-aa0e-8696db0a9c7e"
+        
+        
+        
+ac = require("home/Crafter/Autocraft")
+com = ac.mf.component.proxy(ac.prox.GetProxyByCrafter(""))
+pat = com.getPattern({ name = "" })
