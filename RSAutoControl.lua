c2d3c2125dcc86ad4e4a02710adf6e30efe8e6df
bufu1337 <a.skripkin@hotmail.de>
2022-08-14 03:18:09 +0200

    RS AutoControl

---
+++ RSAutoControl.lua
@@ -0,0 +1,15 @@
+local rsac = {}
+rsac.convert = require("Convert")
+rsac.prox = require("RSProxies")
+rsac.mf = require("MainFunctions")
+rsac.refs = rsac.mf.component.block_refinedstorage_cable
+rsac.items = {}
+rsac.storageitems = rsac.refs.getItems()
+
+for index,item in pairs(rsac.storageitems) do
+    rsac.items[rsac.convert.TextToOName(item)] = {minCount=0, maxCount=0, rschannel={}}
+end
+
+rsac.mf.WriteObjectFile(rsac.items, "/home/RSItems.lua")
+
+return rsac
